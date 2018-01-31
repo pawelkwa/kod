46c279992be8749a25e39ec7090a7324c036e828
pkwasny <pkwasny>
2018-01-31 20:12:48 +0100

    add variables file to the repository

---
+++ day2/variables.py
@@ -0,0 +1,15 @@
+age = 35               #zmienna int
+temperature = 7.4      #zmienna float
+name = 'Pawel'         #zmienna string
+nothing = None         #zmienna nontype
+
+print(name, age)
+print (type(age))
+# print (temperature)
+# print (name)
+# @TODO: wyświetl reszte zmiennych na ekranie
+# print (nothing)
+age = 'bezsensu'
+print (type(age))
+print (type(name))
+print (type(nothing))
